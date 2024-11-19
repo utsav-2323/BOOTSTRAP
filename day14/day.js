6b882a7123c49dc34436c2f6ca0caa9fa5e37b28
utsav patel <utsacbhalodiya2323@gmail.com>
2024-11-19 15:19:19 +0530

    BOOTSTRAP

---
+++ day14/day.js
@@ -0,0 +1,67 @@
+
+
+$(document).ready(function(){
+    $("#add1").mouseenter(function(){
+    $("#a1").show(1100);
+    })
+})
+$(document).ready(function(){
+    $("#add1").mouseleave(function(){
+    $("#a1").hide(1100);
+    })
+})
+
+$(document).ready(function(){
+    $("#add2").mouseenter(function(){
+    $("#a2").show(1100);
+    })
+})
+$(document).ready(function(){
+    $("#add2").mouseleave(function(){
+    $("#a2").hide(1100);
+    })
+})
+
+$(document).ready(function(){
+    $("#add3").mouseenter(function(){
+    $("#a3").show(1100);
+    })
+})
+$(document).ready(function(){
+    $("#add3").mouseleave(function(){
+    $("#a3").hide(1100);
+    })
+})
+
+$(document).ready(function(){
+    $("#add4").mouseenter(function(){
+    $("#a4").show(1100);
+    })
+})
+$(document).ready(function(){
+    $("#add4").mouseleave(function(){
+    $("#a4").hide(1100);
+    })
+})
+
+$(document).ready(function(){
+    $("#add5").mouseenter(function(){
+    $("#a5").show(1100);
+    })
+})
+$(document).ready(function(){
+    $("#add5").mouseleave(function(){
+    $("#a5").hide(1100);
+    })
+})
+
+$(document).ready(function(){
+    $("#add6").mouseenter(function(){
+    $("#a6").show(1100);
+    })
+})
+$(document).ready(function(){
+    $("#add6").mouseleave(function(){
+    $("#a6").hide(1100);
+    })
+})
